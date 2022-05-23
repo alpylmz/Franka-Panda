20ffdbc755f3d53d767869c1ebcac8cd70aa1138
alpylmz <ylmz.alp.e@gmail.com>
2022-05-23 21:09:06 +0300

    side vision integrated

--- franka_example_controllers/scripts/move_to_start.py
+++ franka_example_controllers/scripts/move_to_start.py
@@ -16,11 +16,13 @@ from franka_msgs.srv import SetOrientationCommand
 from franka_msgs.srv import SetChessGoal
 from franka_msgs.srv import SetTrajectoryCommand
 from moveit_msgs.msg import PlanningScene
+from moveit_msgs.msg import RobotTrajectory
 import copy
 from scipy.interpolate import interp1d
 import json
 from functools import partial
 from control_msgs.msg import FollowJointTrajectoryGoal, FollowJointTrajectoryAction
+from trajectory_msgs.msg import JointTrajectory
 
 #from quaternion import Quaternion
 
@@ -45,6 +47,7 @@ is_there_a_goal = False
 is_goal_relative = False
 go_to_our_initial = False
 is_goal_trajectory = False
+go_to_side_vision_init = False
 
 is_goal_jointwise = False
 goal_joint_1 = 0.0
@@ -113,7 +116,7 @@ def joint_move_service(req):
     return True, ""
 
 def move_service(req):
-    global goal_x, goal_y, goal_z, is_there_a_goal, is_goal_relative, goal_q_x, goal_q_y, goal_q_z, goal_q_w, go_to_our_initial, is_goal_jointwise, is_goal_chess_place, is_goal_reached, is_goal_trajectory
+    global goal_x, goal_y, goal_z, is_there_a_goal, is_goal_relative, goal_q_x, goal_q_y, goal_q_z, goal_q_w, go_to_our_initial, is_goal_jointwise, is_goal_chess_place, is_goal_reached, is_goal_trajectory, go_to_side_vision_init
     goal_x = req.x
     goal_y = req.y
     goal_z = req.z
@@ -126,6 +129,7 @@ def move_service(req):
     """
     is_goal_relative = req.is_relative
     go_to_our_initial = req.go_to_init
+    go_to_side_vision_init = req.go_to_side_vision_init
     is_goal_jointwise = False
     is_goal_chess_place = False
     is_goal_trajectory = False
@@ -179,7 +183,7 @@ def addObstacles():
     box_name = "back_obstacle"
     collision_object_left = geometry_msgs.msg.PoseStamped()
     collision_object_left.header.frame_id = "panda_link0"
-    collision_object_left.pose.position.x = -0.30
+    collision_object_left.pose.position.x = -0.50
     collision_object_left.pose.orientation.w = 1.0
     scene.add_box(box_name, collision_object_left, size=(0.1, 10, 10))
 
@@ -274,11 +278,14 @@ if __name__ == '__main__':
 
     #commander.go(wait=True)is_goal_jointwise
     #initial_joints = [0.8807196933525263, -0.8014431536310861, -0.42209067821502666, -2.1911595741634415, -0.33243660358600874, 1.4980820045918766, 1.323550466756026]
-    initial_joints = [0.1525394261890009, -0.08124570311546929, -0.07294265516092496, -1.5018837461405505, 0.006697758157634073, 1.4245549732844034, 0.878600022062846]
+    #initial_joints = [0.1525394261890009, -0.08124570311546929, -0.07294265516092496, -1.5018837461405505, 0.006697758157634073, 1.4245549732844034, 0.878600022062846]
+    initial_joints = [0.15322086521630415, -0.11260720413400414, -0.07933782860502608, -1.1466092779796002, 0.016516494863563113, 1.0695718866963613, 0.8452164789947625]
     #[-0.3426358018005103, -0.7527097266085973, 0.3211428673806972, -2.1997036707603113, 0.22632897217768974, 1.5394524623788894, 0.7070815159243338]
     #initial_joints = [0.19629807516847006, -0.7055285141228648, 0.010857086453996393, -2.2712353889079404, 0.032713600012991166, 1.5440953356424967, 0.9812845707889041]
+    hri_init_joint = [-0.00015594268319585325, -0.7856254591349376, 2.4569001943142155e-05, -2.3559519536390043, 8.081014387428809e-06, 1.5717405031367848, 0.7854055945854093]
+    side_vision_joints = [0.06806257059699611, -1.1706830794400716, 0.03868383648586202, -2.414915495631861, 0.04857524870906982, 1.5837557306289671, 0.8572224840116169]
 
-
+    
     # ----------------------
 
 
@@ -287,7 +294,28 @@ if __name__ == '__main__':
             if not is_there_a_goal:
                 sleep(0.1)
                 continue
-            if is_goal_trajectory:
+            if go_to_side_vision_init:
+                commander.set_joint_value_target(side_vision_joints)
+                plan1 = commander.plan()
+                commander.go(wait=True)
+                go_to_side_vision_init = False
+                is_there_a_goal = False
+                is_goal_reached = True
+                continue
+            elif is_goal_trajectory:
+                # first go to init position
+                rospy.logerr("going to hri init")
+                commander.set_joint_value_target(hri_init_joint)
+                plan = commander.plan()
+                rospy.logerr("planning")
+                commander.go(wait=True)
+                rospy.loginfo("Trajectory goal")
+                rospy.loginfo(goal_trajectory)
+                rospy.loginfo("start?")
+                traj = RobotTrajectory()
+                traj.joint_trajectory = goal_trajectory.trajectory
+                commander.execute(traj, wait=True)
+                is_goal_reached = True
                 #trajectory_client.send_goal(goal_trajectory)
                 continue
             elif is_goal_jointwise:
@@ -317,6 +345,11 @@ if __name__ == '__main__':
                     commander.set_joint_value_target(goal_joints[:7])
                     plan1 = commander.plan()
                     print("Planned and going!")
+                    from pprint import pprint
+                    rospy.logerr("type is")
+                    rospy.logerr(type(plan1))
+                    pprint(plan1)
+                    rospy.logerr(plan1)
                     commander.go(wait=True)
                 except:
                     print("Failed to execute plan...")
@@ -345,7 +378,8 @@ if __name__ == '__main__':
             
             path = commander.retime_trajectory(commander.get_current_state(), path, 0.1)
             from pprint import pprint
-            pprint(path)
+            pprint(vars(path))
+            rospy.logerr(vars(path))
             commander.execute(path, wait=True)
 
             rospy.loginfo("Moving to goal: %f, %f, %f", pose.pose.position.x, pose.pose.position.y, pose.pose.position.z)
